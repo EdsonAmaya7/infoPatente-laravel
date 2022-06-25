494c5c958a8bb633afdd757aab4d84decc4a8c4e
Jisus1840 <delacruzjesus083@gmail.com>
2022-06-25 17:38:36 -0500

    se agrego metodo de agregar patentes, ya funciona, faltan algunas cosas

--- app/Http/Controllers/PatentesController.php
+++ app/Http/Controllers/PatentesController.php
@@ -51,6 +51,36 @@ class PatentesController extends Controller
             $empresa->save();
             return;
         }
+
+        $this->validate($request, [
+            "nombre" => "required | max: 20",
+            // "numero_autores" => "required",
+            "pais_presentacion" => "required",
+            "entidad_pequenia" => "required",
+            "representantes_id" => "required",
+            "tipo" => "required",
+            "aplicacion" => "required",
+            "cecionario_id" => "required",
+            // "tarifa_descuento" => "required",
+        ]);
+
+        // dd($request->all());
+
+        patentes::create([
+            "nombre" => $request->nombre,
+            // "numero_autores" => $request->numero_autores,
+            "pais_presentacion" => $request->pais_presentacion,
+            "entidad_pequenia" => $request->entidad_pequenia,
+            "representantes_id" => $request->representantes_id,
+            "tipo" => $request->tipo,
+            "aplicacion" => $request->aplicacion,
+            "cecionario_id" => $request->cecionario_id,
+            "email_registro" => auth()->user()->email,
+            "autorizacion" => "No",
+            "user_id" => auth()->user()->id
+        ]);
+
+        return redirect()->back()->with(['mensaje'=>'Patente agregada con exito']);
     }
 
     /**
--- resources/views/patentes/create.blade.php
+++ resources/views/patentes/create.blade.php
@@ -22,7 +22,12 @@
                 <div class="col-md-12 text-center mt-4">
                     <h1>Nueva Patente</h1>
                 </div>
-                <form method="POST">
+                @if (session('mensaje'))
+                    <div class="mt-2 p-3 bg-success text-white text-center rounded-pill ">
+                        <div>{{ session('mensaje') }}</div>
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('patentes.store') }}">
                     @csrf
 
                     <div class="row">
@@ -30,12 +35,15 @@
                             <div class="item item-label mb-3" style="margin-left: 150px">
                                 <div class="wrapper mt-4">
                                     <label style="font-size: 20px" for="">Nombre de la Patente</label>
-                                    <input id="nombre_patente" type="nombre_patente" class="form-control"
-                                        name="nombre_patente" value="{{ old('nombre_patente') }}" required>
+                                    <input id="nombre" type="text" class="form-control" name="nombre"
+                                        value="{{ old('nombre') }}">
                                     <div class="d-flex justify-content-end mt-2">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Trate de ser conciso y descriptivo de lo que es o hace la patente. Este nombre no es un nombre de archivo."></i>
                                     </div>
+                                    @error('nombre')
+                                        <p>{{ $message }}</p>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -60,6 +68,9 @@
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Todos los autores son considerados de tener los mismos e iguales derechos."></i>
                                     </div>
+                                    @error('numero_autores')
+                                        <p>{{ $message }}</p>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -68,10 +79,11 @@
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="pais">Pais donde esta patente sera presentada</h4>
-                                    <select name="pais" id="pais" class="form-control" style="width: 60%;">
+                                    <h4 for="">Pais donde esta patente sera presentada</h4>
+                                    <select name="pais_presentacion" id="pais_presentacion" class="form-control"
+                                        style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
-                                        <option value="">Mexico</option>
+                                        <option value="1">Mexico</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
@@ -83,34 +95,41 @@
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="entidad">¿Se clasifica como entidad pequeña?</h4>
-                                    <select name="entidad" id="entidad" class="form-control" style="width: 60%;">
+                                    <h4 for="">¿Se clasifica como entidad pequeña?</h4>
+                                    <select name="entidad_pequenia" id="entidad_pequenia" class="form-control"
+                                        style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
-                                        <option value="">Si</option>
-                                        <option value="">No</option>
+                                        <option value="0">Si</option>
+                                        <option value="1">No</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Una entidad pequeña se define como: persona, empresa pequeña u organización sin fines de lucro"></i>
                                     </div>
                                 </div>
+                                @error('entidad_pequenia')
+                                    <p>{{ $message }}</p>
+                                @enderror
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="representante_legal">¿Desea agregar un representante legal?</h4>
-                                    <select name="representante_legal" id="representante_legal" class="form-control"
+                                    <h4 for="">¿Desea agregar un representante legal?</h4>
+                                    <select name="representantes_id" id="representantes_id" class="form-control"
                                         style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
-                                        <option value="">Si</option>
-                                        <option value="">No</option>
+                                        <option value="1">Si</option>
+                                        <option value="2">No</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Si no deasea agregar representante legal, el primer autor sera considerado como tal"></i>
                                     </div>
                                 </div>
+                                @error('representante_id')
+                                    <p>{{ $message }}</p>
+                                @enderror
                             </div>
                         </div>
                     </div>
@@ -118,50 +137,59 @@
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="tipo_patente">Seleccione tipo de patente</h4>
-                                    <select name="tipo_patente" id="tipo_patente" class="form-control"
-                                        style="width: 60%;">
+                                    <h4 for="">Seleccione tipo de patente</h4>
+                                    <select name="tipo" id="tipo" class="form-control" style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
-                                        <option value="">Utilidad</option>
-                                        <option value="">Diseño</option>
-                                        <option value="">Planta</option>
+                                        <option value="Utilidad">Utilidad</option>
+                                        <option value="Diseño">Diseño</option>
+                                        <option value="Planta">Planta</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Seleccione una opcion para desplegar una pequeña descripcion"></i>
                                     </div>
                                 </div>
+                                @error('tipo')
+                                    <p>{{ $message }}</p>
+                                @enderror
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="tipo_aplicacion">Seleccione tipo de aplicación</h4>
-                                    <select name="tipo_aplicacion" id="tipo_aplicacion" class="form-control"
-                                        style="width: 60%;">
+                                    <h4 for="">Seleccione tipo de aplicación</h4>
+                                    <select name="aplicacion" id="aplicacion" class="form-control" style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
+                                        <option value="nueva">Nueva aplicacion</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Seleccione una opcion para desplegar una pequeña descripcion"></i>
                                     </div>
                                 </div>
+                                @error('aplicacion')
+                                    <p>{{ $message }}</p>
+                                @enderror
                             </div>
                         </div>
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="cesionario">¿Desea agregar un cesionario?</h4>
-                                    <select name="cesionario" id="cesionario" class="form-control" style="width: 60%;">
+                                    <h4 for="">¿Desea agregar un cesionario?</h4>
+                                    <select name="cecionario_id" id="cecionario_id" class="form-control"
+                                        style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
-                                        <option value="">Si</option>
-                                        <option value="">No</option>
+                                        <option value="1">Si</option>
+                                        <option value="2">No</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Una entidad pequeña se define como: persona, empresa pequeña u organización sin fines de lucro"></i>
                                     </div>
                                 </div>
+                                @error('cesionario_id')
+                                    <p>{{ $message }}</p>
+                                @enderror
                             </div>
                         </div>
                     </div>
@@ -169,18 +197,28 @@
                         <div class="col-md-4">
                             <div style="margin-left: 100px">
                                 <div class="">
-                                    <h4 for="" id="tarifa_descuento">Tarifa de descuento</h4>
+                                    <h4 for="">Tarifa de descuento</h4>
                                     <select name="tarifa_descuento" id="tarifa_descuento" class="form-control"
                                         style="width: 60%;">
                                         <option value="" disabled selected>Selecciona una opcion</option>
-                                        <option value="">Si</option>
-                                        <option value="">No</option>
+                                        <option value="0">Si</option>
+                                        <option value="1">No</option>
                                     </select>
                                     <div class="d-flex justify-content-end mt-2" style="width: 60%;">
                                         <i class="fa fa-question" data-bs-toggle="tooltip"
                                             title="Cuando la prestación de los servicios sea solicitada por inventores personas físicas, por micro o pequeñas industrias, por instituciones de educación superior públicas o privadas, por institutos de investigación científica y tecnológica del sector público, podrán pagar únicamente el 50% de las cuotas de la Tarifa."></i>
                                     </div>
                                 </div>
+                                @error('tarifa_descuento')
+                                    <p>{{ $message }}</p>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                            <div style="margin-left: 100px">
+                                <button class="btn btn-danger" type="submit">Enviar</button>
                             </div>
                         </div>
                     </div>
